585b98e10d2e8b3c99bc9b9006a7b69e8370e387
Javi <javdecab@alum.us.es>
2019-12-02 11:14:27 +0100

    Arreglo error test

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://decide-travis-javdecab.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
